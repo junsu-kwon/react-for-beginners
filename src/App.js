ac1a201e03d3b90e767c092320e52b57a2918a3a
junsu-kwon <kjs940322@gmail.com>
2023-02-13 01:04:46 +0900

    230213 공부

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import Detail from './routes/Detail';
 import Home from './routes/Home';
 
 function App() {
-  console.log(process.env.PUBLIC_URL);
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
-        <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
-          element={<Detail />}
-        ></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
       </Routes>
     </Router>
   );
